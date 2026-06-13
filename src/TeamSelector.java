336ce6db89603278d115a25644455465482ce593
TOWUK <towuk@internet.ru>
2026-06-13 22:40:49 +0300

    Update TeamSelector.java

--- src/TeamSelector.java
+++ src/TeamSelector.java
@@ -15,14 +15,17 @@ public class TeamSelector extends Plugin {
     private static final String[][] OPTS = {{PREFIXES[0] + "Sharded", PREFIXES[1] + "Crux"}};
     @Override
     public void init() {
-        menuId = Menus.registerMenu((p, opt) -> {if (opt == 0 || opt == 1) applyTeam(p, opt);});
-        Events.on(EventType.WorldLoadEvent.class, e -> Time.runTask(28f, () -> Groups.player.each(p -> applyTeam(p, 2))));
+        menuId = Menus.registerMenu((p, opt) -> { if (opt == 0 || opt == 1) applyTeam(p, opt); });
+        Events.on(EventType.WorldLoadEvent.class, e -> Time.runTask(30f, () -> Groups.player.each(p -> applyTeam(p, 2))));
         Events.on(EventType.PlayerJoin.class, e -> applyTeam(e.player, 2));
     }
-    private void applyTeam(Player p, int index) {
-        p.team(TEAMS[index]);
-        p.name = PREFIXES[index] + p.name.replace(PREFIXES[0], "").replace(PREFIXES[1], "").replace(PREFIXES[2], "");
-        if (index == 2) {
+    private void applyTeam(Player p, int i) {
+        p.team(TEAMS[i]);
+        String base = p.name;
+        for (var pre : PREFIXES) if (base.startsWith(pre)) { base = base.substring(pre.length()); break; }
+        p.name = PREFIXES[i] + base;
+
+        if (p.con != null && i == 2) {
             Call.setCameraPosition(p.con, Vars.world.unitWidth() / 2f, Vars.world.unitHeight() / 2f);
             Call.menu(p.con, menuId, null, "Нажмите Esc чтобы\nостаться наблюдателем", OPTS);
         }
