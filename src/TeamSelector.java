e04feb50d9d4099f4d2d0996e194f5e8e5dfd80d
TOWUK <towuk@internet.ru>
2026-06-13 11:36:47 +0300

    Update TeamSelector.java

--- src/TeamSelector.java
+++ src/TeamSelector.java
@@ -10,12 +10,12 @@ import mindustry.mod.Plugin;
 import mindustry.ui.Menus;
 public class TeamSelector extends Plugin {
     private int menuId;
-    private static final Team[] TEAMS = { Team.sharded, Team.crux, Team.derelict };
-    private static final String[] PREFIXES = { "[#ffd37f]" + (char)63356, "[#f25555]" + (char)63357, "[#dadada]" };
-    private static final String[][] OPTS = { { PREFIXES[0] + "Sharded", PREFIXES[1] + "Crux" } };
+    private static final Team[] TEAMS = {Team.sharded, Team.crux, Team.derelict};
+    private static final String[] PREFIXES = {"[#ffd37f]\uF77C", "[#f25555]\uF77D", "[#dadada]"};
+    private static final String[][] OPTS = {{PREFIXES[0] + "Sharded", PREFIXES[1] + "Crux"}};
     @Override
     public void init() {
-        menuId = Menus.registerMenu((p, opt) -> { if (opt == 0 || opt == 1) applyTeam(p, opt); });
+        menuId = Menus.registerMenu((p, opt) -> {if (opt == 0 || opt == 1) applyTeam(p, opt);});
         Events.on(EventType.WorldLoadEvent.class, e -> Time.runTask(28f, () -> Groups.player.each(p -> applyTeam(p, 2))));
         Events.on(EventType.PlayerJoin.class, e -> applyTeam(e.player, 2));
     }
@@ -24,7 +24,7 @@ public class TeamSelector extends Plugin {
         p.name = PREFIXES[index] + p.name.replace(PREFIXES[0], "").replace(PREFIXES[1], "").replace(PREFIXES[2], "");
         if (index == 2) {
             Call.setCameraPosition(p.con, Vars.world.unitWidth() / 2f, Vars.world.unitHeight() / 2f);
-            Call.menu(p.con, menuId, null, PREFIXES[2] + "Нажмите Esc чтобы\nостаться наблюдателем", OPTS);
+            Call.menu(p.con, menuId, null, "Нажмите Esc чтобы\nостаться наблюдателем", OPTS);
         }
     }
 }
