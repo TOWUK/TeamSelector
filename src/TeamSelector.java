d9e213a1b60818ad7f3459ef99e4e4a7c209706e
TOWUK <towuk@internet.ru>
2026-08-02 00:49:12 +0300

    минус багоюзеры (цвет-ники и пусто-ники) + новый визуал

--- src/TeamSelector.java
+++ src/TeamSelector.java
@@ -9,14 +9,17 @@ import mindustry.gen.Groups;
 import mindustry.gen.Player;
 import mindustry.mod.Plugin;
 import mindustry.ui.Menus;
+import java.util.regex.Pattern;
+
 public class TeamSelector extends Plugin {
     private int menuId;
     private static final Team[] TEAMS = {Team.sharded, Team.crux, Team.derelict};
     private static final String[] PREFIXES = {"[#ffd37f]\uF77C", "[#f25555]\uF77D", "[#dadada]"};
-    private static final String[][] OPTS = {{PREFIXES[0] + "Sharded", PREFIXES[1] + "Crux"}};
+    private static final String[][] OPTS = {{PREFIXES[0] + "Sharded", PREFIXES[1] + "Crux"}, {PREFIXES[2] + "НАБЛЮДАТЬ"}};
+    private static final Pattern INVISIBLES = Pattern.compile("[\\p{C}\\u00A0\\u2000-\\u200D\\u202F\\u205F\\u3000\\uFEFF\\u3164\\u2800]");
     @Override
     public void init() {
-        menuId = Menus.registerMenu((p, opt) -> { if(opt >= 0 && opt < 2) setTeam(p, TEAMS[opt], false); });
+        menuId = Menus.registerMenu((p, opt) -> { if(opt >= 0 && opt < 3) setTeam(p, TEAMS[opt], false); });
         Events.on(EventType.PlayEvent.class, e -> Time.runTask(44f, () -> Groups.player.each(p -> setTeam(p, Team.derelict, true))));
         Events.on(EventType.PlayerJoin.class, e -> setTeam(e.player, Team.derelict, true));
         Timer.schedule(this::checkTeams, 1f, 1f);
@@ -28,12 +31,15 @@ public class TeamSelector extends Plugin {
     private void setTeam(Player p, Team team, boolean showMenu) {
         int i = idx(team);
         p.team(TEAMS[i]);
-        for(var pre : PREFIXES) if(p.name.startsWith(pre)) { p.name = p.name.substring(pre.length()); break; }
-        p.name = PREFIXES[i] + p.name;
+        String n = INVISIBLES.matcher(p.plainName()).replaceAll("");
+        n = n.trim().isEmpty()
+            ? "MindCup_" + Integer.toString((p.uuid().hashCode() & 0x7FFFFFFF) % 45360 + 1296, 36)
+            : n.substring(n.charAt(0) == '\uF77C' || n.charAt(0) == '\uF77D' ? 1 : 0);
+        p.name = PREFIXES[i] + n;
         if(p.unit() != null) p.unit().kill();
         if(i == 2 && showMenu && p.con != null) {
-            Call.setCameraPosition(p.con, Vars.world.unitWidth() / 2f, Vars.world.unitHeight() / 2f);
-            Call.menu(p.con, menuId, null, "Нажмите Esc чтобы\nостаться наблюдателем", OPTS);
+            Call.setCameraPosition(p.con, Vars.world.unitWidth() * 0.5f, Vars.world.unitHeight() * 0.5f);
+            Call.menu(p.con, menuId, null, "\nВЫБЕРИ КОМАНДУ\nили нажми ESC/НАБЛЮДАТЬ\nчтобы смотреть игру!\n\n", OPTS);
         }
     }
     private int idx(Team t) { return t == Team.sharded ? 0 : t == Team.crux ? 1 : 2; }
